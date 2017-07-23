7d391052ce6652c965d9eaf4e068ef5334233e68
Jessica-Koch <jessicakoch136@icloud.com>
2017-07-23 00:15:09 -0700

    can get mentor schedule

--- lib/trapperKeeper.rb
+++ lib/trapperKeeper.rb
@@ -24,6 +24,12 @@ class TrapperKeeper
   def get_me
     response = self.class.get("#{@base_url}/users/me", headers: { "authorization": @auth_token })
     JSON.parse(response.body)
+  end
 
+  def get_mentor_availability
+    mentor = self.get_me.dig('current_enrollment', 'mentor_id')
+    response = self.class.get("#{@base_url}/mentors/#{mentor}/student_availability", headers: {"authorization": @auth_token})
+
+    # JSON.parse(response.body)
   end
 end
