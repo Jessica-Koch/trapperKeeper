c9746aa19a9a49e4ca32fc71c13afbb30b851c35
Jessica-Koch <jessicakoch136@icloud.com>
2017-07-23 21:34:03 -0700

    create submissions

--- lib/trapperKeeper.rb
+++ lib/trapperKeeper.rb
@@ -50,4 +50,21 @@ class TrapperKeeper
       "stripped-text": body_text
     }, headers: {'authorization': @auth_token})
   end
-end
+
+  def create_submission(assignment_branch=nil, assignment_commit_link=nil, checkpoint_id, comment)
+    enrollment_id = self.get_me.dig("current_enrollment", "id")
+    self.class.post("#{@base_url}/checkpoint_submissions",
+      body: {
+        "assignment_branch": assignment_branch,
+        "assignment_commit_link": assignment_commit_link,
+        "checkpoint_id": checkpoint_id,
+        "comment": comment,
+        "enrollment_id": enrollment_id
+      },
+      headers: {
+        "authorization": @auth_token
+      })
+      # >> kele_client.create_submission(checkpoint_id, assignment_branch, assignment_commit_link, comment)
+
+    end
+  end
